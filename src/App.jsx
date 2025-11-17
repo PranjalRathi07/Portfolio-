9b7a17ba9bf82dcc67fd56dbcb9577cd7eedfe12
Pranjal Rathi <rathipranjal123@gmail.com>
2025-11-18 01:42:41 +0530

    install tailwind and project section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Hero from "./components/hero";
 import Aboutme from "./components/aboutme";
 import Skills from "./components/skills";
+import Projects from "./components/projects";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Hero></Hero>
       <Aboutme></Aboutme>
       <Skills></Skills>
+      <Projects></Projects>
     </div>
   );
 }
